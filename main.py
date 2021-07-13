e615da52fbc3742501cfd7c64ed294dcb5c4b2a4
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-07-13 17:40:46 -0400

    implemented function to encrypt message

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ df = pd.DataFrame(data=encryptionKey)
 df['Character'] = df['Character'].astype(str)
 df['Byte'] = df['Byte'].astype(str)
 
-#print(df)
-
 
 def split(message):
     message = str(message)
@@ -18,4 +16,24 @@ def split(message):
     return array
 
 
-print(split("33" + " he"))
+def encrypt(message):
+    message = split(message)
+    encoded_message = ""
+
+    for i in range(len(message)):
+        curr_char = message[i]
+
+        try:
+            encoded_char = df.loc[df['Character'] == curr_char, 'Byte'].iloc[0]
+
+        except:
+            print('unrecognized character')
+            encoded_char = '@@@'
+
+        encoded_message = encoded_message + encoded_char
+    return encoded_message
+
+
+print(encrypt("个"))
+
+# check for empty message
