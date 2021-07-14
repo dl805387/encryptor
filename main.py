dfad72e65e017afb952353109e74cab2160a2b38
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-07-14 00:50:14 -0400

    caesar cipher for decryption

--- main.py
+++ main.py
@@ -8,23 +8,44 @@ df['Character'] = df['Character'].astype(str)
 df['Byte'] = df['Byte'].astype(str)
 
 
+# the caesar cipher shifts the encryption key by 4 rows down
+# the 4 rows at the end of the key cannot be shifted down so it starts from the top
 def caesar_cipher(char):
-    # the caesar cipher shifts the encryption key by 4 characters down
-    # the 4 characters at the end of the key cannot be shifted down so it starts from the top
+    # finds the encoded byte that is associated with the character
     if char == "Ѕ":
-        shifted_code = df.loc[df['Character'] == " ", 'Byte'].iloc[0]
+        encoded_byte = df.loc[df['Character'] == " ", 'Byte'].iloc[0]
     elif char == "І":
-        shifted_code = df.loc[df['Character'] == "!", 'Byte'].iloc[0]
+        encoded_byte = df.loc[df['Character'] == "!", 'Byte'].iloc[0]
     elif char == "Ї":
-        shifted_code = df.loc[df['Character'] == '"', 'Byte'].iloc[0]
+        encoded_byte = df.loc[df['Character'] == '"', 'Byte'].iloc[0]
     elif char == "Ј":
-        shifted_code = df.loc[df['Character'] == "#", 'Byte'].iloc[0]
+        encoded_byte = df.loc[df['Character'] == "#", 'Byte'].iloc[0]
     else:
-        shifted_code = df.loc[df['Character'].shift(4) == char, 'Byte'].iloc[0]
+        encoded_byte = df.loc[df['Character'].shift(4) == char, 'Byte'].iloc[0]
 
-    return shifted_code
+    return encoded_byte
 
 
+# shifts the encryption key by 4 rows up
+# the 4 rows at the top of the key cannot be shifted up so it starts from the bottom
+def caesar_decode(encoded_byte):
+    # finds the row that contains the correct encoded byte
+    if encoded_byte == "20":
+        shifted_row = df[df.eq("d085").any(1)]
+    elif encoded_byte == "21":
+        shifted_row = df[df.eq("d086").any(1)]
+    elif encoded_byte == "22":
+        shifted_row = df[df.eq("d087").any(1)]
+    elif encoded_byte == "23":
+        shifted_row = df[df.eq("d088").any(1)]
+    else:
+        shifted_row = df[df.shift(-4).eq(encoded_byte).any(1)]
+
+    # returns the character from the row
+    return shifted_row['Character'].tolist()[0]
+
+
+# uses UTF-8 encoding and caesar cipher
 def encrypt(message):
     message = str(message)
     if message == "":
@@ -34,13 +55,11 @@ def encrypt(message):
 
     for char in message:
         try:
-            # finds the encoded byte that is associated with the character
-            encoded_char = caesar_cipher(char)
+            encoded_message += caesar_cipher(char)
 
         except:
             return "unrecognized character, cannot encrypt"
 
-        encoded_message += encoded_char
     return encoded_message
 
 
@@ -53,30 +72,24 @@ def decrypt(message):
     i = 0
     while i < len(message):
         try:
-            curr_byte = ""
-            # encoded byte can either be 2 characters or 4
+            encoded_byte = ""
+            # encoded byte can either be 2 or 4 characters
             # we know that it will be 4 if the encoded byte starts with "c" or "d"
             if message[i] == "c" or message[i] == "d":
-                curr_byte += message[i]
-                curr_byte += message[i + 1]
-                curr_byte += message[i + 2]
-                curr_byte += message[i + 3]
+                encoded_byte += message[i]
+                encoded_byte += message[i + 1]
+                encoded_byte += message[i + 2]
+                encoded_byte += message[i + 3]
 
-                # .eq is used to find the row that contains the correct byte
-                row = df[df.eq(curr_byte).any(1)]
-                # gets the character from the row
-                curr_char = row['Character'].tolist()[0]
-                decoded_message += curr_char
+                decoded_char = caesar_decode(encoded_byte)
+                decoded_message += decoded_char
                 i += 4
             else:
-                curr_byte += message[i]
-                curr_byte += message[i + 1]
+                encoded_byte += message[i]
+                encoded_byte += message[i + 1]
 
-                # .eq is used to find the row that contains the correct byte
-                row = df[df.eq(curr_byte).any(1)]
-                # gets the character from the row
-                curr_char = row['Character'].tolist()[0]
-                decoded_message += curr_char
+                decoded_char = caesar_decode(encoded_byte)
+                decoded_message += decoded_char
                 i += 2
         except:
             return "error, cannot decrypt"
@@ -84,9 +97,8 @@ def decrypt(message):
     return decoded_message
 
 
-
-
-
-# Є   Ѕ І Ї Ј
-print(encrypt("Ј"))
-print(caesar_cipher("Ј"))
+# testing
+print(encrypt("my name is danny ! ЈЇJ"))
+print(decrypt("717d2472657169246d7724686572727d24252423224e"))
+print(encrypt("my name is danny ! ЈЇJ五"))
+print(decrypt("717d2472657169246d7724686572727d24252423224e五"))
